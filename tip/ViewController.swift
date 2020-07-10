8e55b5c9f1c84a96da8e6efd5f6bda08410bf06f
tnguyen98 <32310508+tnguyen98@users.noreply.github.com>
2020-07-10 14:21:05 -0400

    Refactored calculation in ViewController

--- tip/ViewController.swift
+++ tip/ViewController.swift
@@ -39,43 +39,38 @@ class ViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        let bill = Double(billAmountTextField.text!) ?? 0
         tipControl.selectedSegmentIndex = UserDefaults.standard.integer(forKey: "index")
-        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        calculation(percent: tipPercentages[tipControl.selectedSegmentIndex])
+        
+        
+    }
+    
+    func calculation(percent: Double){
+        let bill = Double(billAmountTextField.text!) ?? 0
+        print(bill)
+        let tip = bill * percent
         let total = bill + tip
-        percentLabel.text = percentFormatter.string(from: NSNumber(value: tipPercentages[tipControl.selectedSegmentIndex]))
+        percentLabel.text = percentFormatter.string(from: NSNumber(value: percent))
         tipAmountLabel.text = currencyFormatter.string(from: NSNumber(value: tip))
         totalLabel.text = currencyFormatter.string(from: NSNumber(value: total))
     }
-    
     @IBAction func onTap(_ sender: Any) {
         view.endEditing(true)
     }
     
     @IBAction func customTipSubmit(_ sender: Any) {
-        let bill = Double(billAmountTextField.text!) ?? 0
-        var percentage = tipPercentages[tipControl.selectedSegmentIndex]
         if(customTip.text != ""){
-            percentage = (Double(customTip.text!) ?? 0) / 100
+            calculation(percent: (Double(customTip.text!) ?? 0) / 100)
+        } else {
+            calculation(percent: tipPercentages[tipControl.selectedSegmentIndex])
         }
-        let tip = bill * percentage
-        let total = bill + tip
         customTip.text = ""
-        percentLabel.text = percentFormatter.string(from: NSNumber(value: percentage))
-        tipAmountLabel.text = currencyFormatter.string(from: NSNumber(value: tip))
-        totalLabel.text = currencyFormatter.string(from: NSNumber(value: total))
     }
     
     @IBAction func calculateTip(_ sender: Any) {
-        let bill = Double(billAmountTextField.text!) ?? 0
-        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
-        let total = bill + tip
-        percentLabel.text = percentFormatter.string(from: NSNumber(value: tipPercentages[tipControl.selectedSegmentIndex]))
-        tipAmountLabel.text = currencyFormatter.string(from: NSNumber(value: tip))
-        totalLabel.text = currencyFormatter.string(from: NSNumber(value: total))
-//        tipAmountLabel.text = String(format: "$%.2f", tip)
-//        totalLabel.text = String(format: "$%.2f", total)
+        calculation(percent: tipPercentages[tipControl.selectedSegmentIndex])
     }
+    
     
 }
 
